106a5f239d6508b5bca9a92d97a4606bb69682d1
Ansh <neetusharma981074@gmail.com>
2022-01-21 13:25:07 +0530

    dark page yayyyyy

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,10 +4,10 @@ import Link from 'next/link'
 const Header = () => {
   
     return (
-      <header className='sticky'>
-      <nav className="container flex items-center py-4 mt-4 sm:mt-12">
+      <header className='sticky bg-black top-0 z-50'>
+      <nav className="container flex items-center py-4 ">
         <div className="py-1 w-8 h-8"><img src="/img/logo.png" alt="" /></div>
-        <ul className="flex flex-1 justify-end items-center gap-12 text-bookmark-blue uppercase text-xs flex-wrap">
+        <ul className="flex flex-1 justify-end items-center gap-12 text-bookmark-white uppercase text-xs flex-wrap">
           <Link className="cursor-pointer" href='/'>Home</Link>
           <Link className="cursor-pointer" href='/blog'>Blog</Link>
           <Link className="cursor-pointer" href='/#subscribe'>Subscribe</Link>
