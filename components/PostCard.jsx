f6755bcb5c42d21270a1353444f4e0c14554fe49
Ansh <neetusharma981074@gmail.com>
2022-02-02 11:20:03 +0530

    redesign blog card

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,43 +6,16 @@ import Link from 'next/link';
 import { grpahCMSImageLoader } from '../util.js';
 
 const PostCard = ({ post, anim }) => (
-  <div className="shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8 bg-[#131313] shadow-black" data-aos={anim()}>
-    <div className="relative overflow-hidden shadow-md pb-80 mb-6 w-auto h-auto ">
-      <img src={post.featuredImage.url} alt="" className="object-center object-contain w-full h-full absolute shadow-lg rounded-t-lg lg:rounded-lg" />
-    </div>
-
-    <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-teal-400 text-3xl font-semibold">
-      <Link href={`/post/${post.slug}`}>{post.title}</Link>
-    </h1>
-    <p className="text-gray-200 text-center mb-4">{post.exerpt}</p>
-    <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
-      <div className="flex justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
-        <Image
-          unoptimized={true}
-          loader={grpahCMSImageLoader}
-          alt={post.author.name}
-          height="30px"
-          width="30px"
-          className="align-middle rounded-full"
-          src={post.author.photo.url}
-        />
-        <p className="inline align-middle text-gray-200 ml-2 font-medium text-lg">{post.author.name}</p>
+  <div className="flex flex-col items-centerrounded-lg border shadow-md md:flex-row md:max-w-xl border-gray-700 bg-gray-800 mx-auto mt-10 overflow-hidden" data-aos={anim()}>
+    <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={post.featuredImage.url} alt=""/>
+      <div className="flex flex-col justify-between p-4 leading-normal">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">{post.title}</h5>
+        <p className="mb-3 font-normal text-gray-400">{post.exerpt}</p>
+        <span>Author - {post.author.name}</span>
+        <Link href="/post/[slug]" as={`/post/${post.slug}`}>
+          <a className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white transition-all duration-500 hover:border-transparent hover:text-teal-500 hover:bg-white mt-4" style={{maxWidth: '120px', minWidth: '120px'}}>Read more</a>
+        </Link>
       </div>
-      <div className="font-medium text-gray-200">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-teal-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-        </svg>
-        <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
-      </div>
-    </div>
-    <p className="text-center text-lg text-gray-400 font-normal px-4 lg:px-20 mb-8">
-      {post.excerpt}
-    </p>
-    <div className="text-center">
-      <Link href={`/post/${post.slug}`}>
-        <span className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-teal-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Continue Reading</span>
-      </Link>
-    </div>
   </div>
 );
 
